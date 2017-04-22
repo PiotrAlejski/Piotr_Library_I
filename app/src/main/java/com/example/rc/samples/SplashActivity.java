d0d1993a5e2ae62c97c716a5912700a37927d13b
unknown <piotr.alejski@gmail.com>
2017-04-22 11:00:08 +0200

    add splash activity and login

---
+++ app/src/main/java/com/example/rc/samples/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.rc.samples;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final String TOKEN = "token";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//        prefs
+//                .edit()
+//                .putString(TOKEN, "jakiś token")
+//                .commit();
+        Intent intent = null;
+        if(!prefs.contains(TOKEN)){
+            intent = new Intent(this, MainActivity.class);
+
+        } else {
+            //Login_Activity
+            intent = new Intent(this, LoginActivity.class);
+
+        }
+        startActivity(intent);
+        finish();
+
+
+    }
+}
